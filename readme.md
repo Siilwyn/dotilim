ed7fbc00b5ecbf2e29ca5e9c4208eb70ddc71c76
Selwyn <talk@selwyn.cc>
2020-03-10 14:37:22 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,81 @@
+# Dotilim
+Changing desktop wallpapers, at an interval, on your favorite Linux distribution. The goal of this project is to get a dotfile friendly configuration standard for wallpapers.
+
+To achieve this, the idea is to:  
+A) Define a wallpaper configuration specification  
+B) Provide a CLI in Rust to use.  
+C) Provide a reference implementation library in Rust.  
+
+> Currently the project is mainly focused on B, the CLI, consider the whole project alpha.
+
+## Configuration
+Located in `$XDG_CONFIG_HOME/dotilim.toml` or `~/.config/dotilim.toml`, an example:
+```toml
+version = 1
+sources = ["~/Pictures/Wallpapers/**/*.jpg"]
+duration = 60
+order = "random"
+```
+
+### Fields
+#### `version`
+The configuration version, latest at the moment is: `1`.
+#### `sources`
+A list of paths to your wallpapers, an item can also use unix shell style globs.
+#### `duration`
+Number of seconds a wallpaper is shown before changing if multiple wallpapers are given.
+#### `order`
+Either `Alphabetical` or `Random`.
+
+## Running as a system service
+To run Dotilim in the background automatically: Place the following content in either `/etc/systemd/user/dotilim.service` or `~/.config/systemd/user/dotilim.service`. Packagers of systemd-based distributions are encouraged to include the file in the former location.
+
+```toml
+[Unit]
+Description=dotilim
+Documentation=https://github.com/Siilwyn/dotilim
+
+[Service]
+ExecStart=/usr/bin/dotilim
+Restart=always
+RestartSec=12
+
+[Install]
+WantedBy=default.target
+```
+
+The following example commands will run the service once and enable the service to always run on login in the future respectively:
+
+```
+systemctl --user start dotilim.service
+systemctl --user enable dotilim.service
+```
+
+## Specification
+> Version 1
+### Configuration types
+```
+version: Integer
+sources: Array<String>
+duration: Integer
+order: Enum { random, alphabetical }
+```
+
+#### Field parsing
+### `sources`
+Input: Array containing (glob) file paths.
+Valid output: Each path shell resolved and every glob path expanded filling a new array of paths.
+Error output: Array of errors, each error matching an incorrect path.
+### `duration`
+Input: Integer of seconds a wallpaper is shown before changing if multiple wallpapers are given.
+Valid output: Integer, same as input.
+Error output: Parsing error containing the given and expected type.
+### `order`
+Input: Enum of valid orders.
+Valid output: Order Enum, same as input.
+Error output: Parsing error containing the given and expected type.
+
+___
+
+*What’s in a name?  
+The project name Dotilim is a twist on the word 'dotillism' which is the  art of painting in dots. Instead Dotilim works with pixels on your desktop background...*
---
+++ src/main.rs
@@ -0,0 +1,149 @@
+#[macro_use]
+extern crate crossbeam;
+use crossbeam::crossbeam_channel;
+use dirs;
+use glob::glob;
+use rand::seq::IteratorRandom;
+use serde::Deserialize;
+use shellexpand;
+
+use std::fs;
+use std::path;
+use std::process::Command;
+use std::time::Duration;
+
+#[derive(Deserialize, Debug)]
+#[allow(non_camel_case_types)]
+enum Order {
+    random,
+    alphabetical,
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    version: u64,
+    sources: Vec<String>,
+    duration: u64,
+    order: Order,
+}
+
+fn main() {
+    let config = dirs::config_dir()
+        .map(|config_dir| read_config(config_dir.join("dotilim.toml")))
+        .unwrap();
+
+    let wallpapers = expand_sources(config.sources);
+
+    let ticker = crossbeam_channel::tick(Duration::from_secs(config.duration));
+
+    loop {
+        select! {
+            recv(ticker) -> _ => {
+                match config.order {
+                    Order::random => {
+                        change_wallpaper(
+                            &pick_random(&wallpapers).into_os_string().into_string().unwrap()
+                        )
+                    }
+                    Order::alphabetical => {
+                        change_wallpaper(
+                            &wallpapers.iter().next().unwrap().clone().into_os_string().into_string().unwrap()
+                        )
+                    }
+                }
+            },
+        };
+    }
+}
+
+fn change_wallpaper(background_uri: &str) {
+    Command::new("gsettings")
+        .args(&[
+            "set",
+            "org.gnome.desktop.background",
+            "picture-uri",
+            &background_uri,
+        ])
+        .status()
+        .expect("Failed to change wallpaper using Gnome.");
+
+    println!("Set wallpaper to {}", background_uri);
+}
+
+fn read_config(path: path::PathBuf) -> Config {
+    let toml_str = fs::read_to_string(path).expect("No config file found");
+
+    toml::from_str(&toml_str).unwrap()
+}
+
+fn expand_sources(sources: Vec<String>) -> Vec<path::PathBuf> {
+    let paths: Vec<_> = sources
+        .iter()
+        .map(|source| shellexpand::full(source).unwrap())
+        .map(|source| glob(&source).unwrap().filter_map(Result::ok))
+        .flatten()
+        .collect();
+
+    paths
+}
+
+fn pick_random(paths: &Vec<path::PathBuf>) -> path::PathBuf {
+    let mut rng = rand::thread_rng();
+
+    let random_item = paths.iter().choose(&mut rng).unwrap();
+
+    random_item.to_path_buf()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_config_simple() {
+        assert_eq!(
+            read_config(path::PathBuf::from("./test/config-simple.toml")).version,
+            1
+        );
+
+        assert_eq!(
+            read_config(path::PathBuf::from("./test/config-simple.toml")).sources,
+            [String::from("./sources-fixture/*.jpg")]
+        );
+    }
+
+    #[test]
+    fn expand_sources_globs() {
+        assert_eq!(
+            expand_sources(vec![String::from("./test/sources-fixture/*.jpg")]),
+            [
+                path::PathBuf::from("test/sources-fixture/a.jpg"),
+                path::PathBuf::from("test/sources-fixture/b.jpg")
+            ]
+        )
+    }
+
+    #[test]
+    fn expand_sources_multiple_globs() {
+        assert_eq!(
+            expand_sources(vec![
+                String::from("./test/sources-fixture/*.jpg"),
+                String::from("./test/sources-fixture/more/*.jpg")
+            ]),
+            [
+                path::PathBuf::from("test/sources-fixture/a.jpg"),
+                path::PathBuf::from("test/sources-fixture/b.jpg"),
+                path::PathBuf::from("test/sources-fixture/more/a.jpg"),
+                path::PathBuf::from("test/sources-fixture/more/c.jpg")
+            ]
+        )
+    }
+
+    #[test]
+    fn pick_random_simple() {
+        assert_eq!(
+            pick_random(&[path::PathBuf::from("test/sources-fixture/a.jpg")].to_vec()),
+            path::PathBuf::from("test/sources-fixture/a.jpg")
+        )
+    }
+}
